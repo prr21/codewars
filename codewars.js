c1cec8426890caf1bb0e1314f896b176be6536ff
prr <Mr.gustav009@gmail.com>
2019-04-07 22:17:03 +0300

    Функция "Moving Zeros To The End"

--- codewars.js
+++ codewars.js
@@ -1,6 +1,6 @@
 describe('Stop gninnipS My sdroW!', () => {
-	describe('Write a function that takes in a string of one or more words, and returns the same string, but with all five or more letter words reversed (Just like the name of this Kata). Strings passed in will consist of only letters and spaces. Spaces will be included only when more than one word is present.', 
-	() => {
+	describe('Write a function that takes in a string of one or more words, and returns the same string, but with all five or more letter words reversed (Just like the name of this Kata). Strings passed in will consist of only letters and spaces. Spaces will be included only when more than one word is present.'
+	,() => {
 		it('"Welcome" должно выдать строку "emocleW"', () => {
 			assert.equal(spinWords("Welcome"), "emocleW")});
 
@@ -23,3 +23,34 @@ describe('Stop gninnipS My sdroW!', () => {
 			assert.equal(spinWords("Seriously this is the last one"), "ylsuoireS this is the last one")});
 	})
 })
+
+describe('Moving Zeros To The End', () => {
+	describe('Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements.'
+	,() => {
+		it('Все нули должны быть в конце', () => {
+			assert.equal(
+				JSON.stringify( moveZeros([1,2,0,1,0,1,0,3,0,1]) ),
+			 	JSON.stringify([ 1, 2, 1, 1, 3, 1, 0, 0, 0, 0 ]));
+		});
+		it('Все нули должны быть в конце', () => {
+			assert.equal(
+				JSON.stringify( moveZeros([9,0,0,9,1,2,0,1,0,1,0,3,0,1,9,0,0,0,0,9]) ),
+			 	JSON.stringify([9,9,1,2,1,1,3,1,9,9,0,0,0,0,0,0,0,0,0,0]));
+		});
+		it('Все нули должны быть в конце', () => {
+			assert.equal(
+				JSON.stringify( moveZeros(["a",0,0,"b","c","d",0,1,0,1,0,3,0,1,9,0,0,0,0,9]) ),
+			 	JSON.stringify(["a","b","c","d",1,1,3,1,9,9,0,0,0,0,0,0,0,0,0,0]));
+		});
+		it('Все нули должны быть в конце', () => {
+			assert.equal(
+				JSON.stringify( moveZeros(["a",0,0,"b",null,"c","d",0,1,false,0,1,0,3,[],0,1,9,0,0,{},0,0,9]) ),
+			 	JSON.stringify(["a","b",null,"c","d",1,false,1,3,[],1,9,{},9,0,0,0,0,0,0,0,0,0,0]));
+		});
+		it('Все нули должны быть в конце', () => {
+			assert.equal(
+				JSON.stringify( moveZeros([0,1,null,2,false,1,0]) ),
+			 	JSON.stringify([1,null,2,false,1,0,0]));
+		});
+	});
+});
--- codewars.test.js
+++ codewars.test.js
@@ -1,3 +1,4 @@
+//https://www.codewars.com/kata/stop-gninnips-my-sdrow/train/javascript
 function spinWords(string){
   let arr = string.split(' ');
 
@@ -10,3 +11,14 @@ function spinWords(string){
   }
   return arr.join(' ');
 }
+
+//https://www.codewars.com/kata/moving-zeros-to-the-end/train/javascript
+var moveZeros = function (arr) {
+    let zeros = [],
+        other = [];
+
+    for(let i = 0; i < arr.length; i++){
+        arr[i] === 0 ? zeros.push(arr[i]): other.push(arr[i]);
+    }
+    return other.concat(zeros)
+};
